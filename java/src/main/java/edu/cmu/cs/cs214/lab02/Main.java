d6b2e963790d6a264f6c93d9523a1b94e46a3d4b
Jinyan Zhu <jinyanzhu@jinyans-mbp.wifi.local.cmu.edu>
2024-01-22 15:12:57 -0500

    Java: modify Main&Renderer, replace Rectangle with Shape

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,10 +1,11 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
 
         Renderer renderer = new Renderer(rectangle);
 
